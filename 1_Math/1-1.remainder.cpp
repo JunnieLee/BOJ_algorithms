ac4e8df46fc1ac8eee4cc97d830f7e31c7fc9195
JunnieLee <heunghari@naver.com>
2019-07-23 14:36:47 +0900

    the very first commit

---
+++ 1_Math/1-1.remainder.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/10430
+// 첫째 줄에 (A+B)%C, 둘째 줄에 (A%C + B%C)%C, 셋째 줄에 (A×B)%C, 넷째 줄에 (A%C × B%C)%C를 출력한다.
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int A,B,C;
+    cin>>A>>B>>C;
+    
+    cout<< (A+B)%C <<endl;
+    cout<< (A%C + B%C)%C <<endl;
+    
+    cout<< (A*B)%C <<endl;
+    cout<< (A%C * B%C)%C <<endl;
+    
+  
+    return 0;
+}
